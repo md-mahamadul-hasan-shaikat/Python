1c5a2b0a9be84af2c6230d26c09ae4025360927a
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-09 00:00:20 +0600

    User input

---
+++ user_input.py
@@ -0,0 +1,10 @@
+
+name = input("Enter Your Name : ")
+age = input("Enter your Age : ")
+gpa = input("Enter Your GPA : ")
+
+print("\nStudent Informations\n")
+
+print("Name : " + name)
+print("Age : "+ age)
+print("GPA : "+ gpa)
