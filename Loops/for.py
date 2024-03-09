7f7acc77695b620be36d4e5ff029729790abcef2
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-09 12:52:37 +0600

    For Loop

--- Loops/for.py
+++ Loops/for.py
@@ -1,2 +1,25 @@
+'''
 for i in range(10):
     print("MHS")
+
+   
+
+num = [10,20,30,40,50,60,70]
+print(num)
+
+index = 0
+n = len(num)
+while index < n :
+    print(num[index])
+    index = index + 1
+
+ '''
+
+
+num = [10,20,30,40,50,60,70]
+print(num)
+Sum = 0
+for i in num:
+    Sum = Sum + i
+
+print(Sum)
