c08980f20eb9b907dcb554ee204732386e3d46f8
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-09 12:33:46 +0600

    List

---
+++ List/list1.py
@@ -0,0 +1,18 @@
+subjects = ['c','c++', 'Java', 'Python','Dart','javascript','Flutter']
+
+print(len(subjects))
+
+'''
+subjects.append("MHS")
+subjects.insert(2,"Mac OS")
+subjects.remove("Java")
+subjects.sort()
+subjects.reverse()
+subjects.pop()
+subjects.clear()
+subjects.index(3)
+
+'''
+
+post = subjects.count(4)
+print(post)
