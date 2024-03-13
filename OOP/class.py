076a1ae9dc34f33e1485723b80c15a637bbf36ba
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-13 23:45:45 +0600

    Class

---
+++ OOP/class.py
@@ -0,0 +1,22 @@
+
+class Student:
+    roll = ""
+    gpa = ""
+    
+    
+Robiul = Student()
+Robiul.roll = 1
+Robiul.gpa = 3.1
+print(f"Roll : {Robiul.roll}, GPA : {Robiul.gpa}")
+
+
+Rabbi = Student()
+Rabbi.roll = 2
+Rabbi.gpa = 3.2
+print(f"Roll : {Rabbi.roll}, GPA : {Rabbi.gpa}")
+
+
+MHS = Student()
+MHS.roll = 3
+MHS.gpa = 3.3
+print(f"Roll : {MHS.roll}, GPA : {MHS.gpa}")
