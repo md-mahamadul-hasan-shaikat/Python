c3100df53bb16e2794cbcfcf91fc3dfa37da05c1
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-03-13 23:16:47 +0600

    Read File

--- Reading a File/Read/read.py
+++ Reading a File/Read/read.py
@@ -1,7 +1,12 @@
 file = open("student.txt", "r+")
 #print(file.readable())
+'''
 text = file.readline()
 print(text)
 size = len(text)
 print(size)
+'''
+
+for line in file:
+    print(line)
 file.close()
