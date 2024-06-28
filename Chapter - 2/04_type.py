3eda09cfe3587e374e73b9777544ba353a4557ae
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-06-29 01:19:19 +0600

    Type

---
+++ Chapter - 2/04_type.py
@@ -0,0 +1,23 @@
+a = 31
+t = type(a) # class <int>
+print(t)
+
+a = 31.3
+t = type(a) # class <float>
+print(t)
+
+a = "MHS"
+t = type(a) # class <String>
+print(t)
+
+a = 32
+b = str(a) # integer to string conversion
+t = type(b)
+print(t)
+
+a = '32.3'
+b = float(a) # string to float conversion
+t = type(b)
+print(t)
+
+
