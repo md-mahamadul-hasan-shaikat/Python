7b06eaa1fe5bcb829780632c4cf4fe1db023803f
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-06-29 01:51:44 +0600

    Write a python program to calculate the squre of a number entered by the user.

---
+++ Chapter - 2/Practice Que/06_problem.py
@@ -0,0 +1,4 @@
+#Write a python program to calculate the squre of a number entered by the user.
+
+a = int(input('Enter your number : '))
+print('Squre of the numbers is : ', a**2) # a to the power 2
