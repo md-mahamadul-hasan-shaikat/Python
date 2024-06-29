68621b11647ff808bbb41afb2c10bb108098dc2e
md-mahamadul-hasan-shaikat <113624647+md-mahamadul-hasan-shaikat@users.noreply.github.com>
2024-06-29 20:59:28 +0600

    string functions

---
+++ Chapter - 3/02_str_function.py
@@ -0,0 +1,8 @@
+name = 'SHAIKAT'
+
+print(len(name))
+print(name.endswith('KAT'))
+print(name.startswith('SHA'))
+print(name.capitalize())
+replaceName = name.replace('SHAIKAT','MHS')
+print(replaceName)
